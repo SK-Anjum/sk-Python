2d41edef6732f0a24ccff4d9112b20de6651697a
SK-Anjum <161378654+SK-Anjum@users.noreply.github.com>
2025-04-14 18:13:11 +0500

    Update main.py

--- BMI Calculator/main.py
+++ BMI Calculator/main.py
@@ -1,22 +1,22 @@
 import streamlit as st
 
-# App setup
+
 st.set_page_config(page_title="BMI Calculator", page_icon="💪")
 st.title("💪 BMI Calculator")
 st.write("Enter your height and weight to calculate your Body Mass Index (BMI).")
 
-# Input fields
+# Inputs
 input_height_cm = st.number_input("Height (in centimeters):", min_value=30.0, format="%.1f")
 input_weight_kg = st.number_input("Weight (in kilograms):", min_value=1.0, format="%.2f")
 
-# BMI Calculation
+# BMI Cal
 if st.button("Calculate BMI"):
     if input_height_cm > 0 and input_weight_kg > 0:
         height_in_meters = input_height_cm / 100  # Convert cm to meters
         calculated_bmi = input_weight_kg / (height_in_meters ** 2)
         st.success(f"Your BMI is: {calculated_bmi:.2f}")
 
-        # BMI Category
+        # Categry
         if calculated_bmi < 18.5:
             st.warning("You're underweight. ")
         elif 18.5 <= calculated_bmi < 24.9:
